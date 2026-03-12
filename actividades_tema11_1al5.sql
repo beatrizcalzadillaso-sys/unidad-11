06b667aefef083aaec39fa66253e26e882fa657c
beatrizcalzadillaso <beatriz.calzadillaso@elorrieta-errekamari.com>
2026-03-12 21:17:48 +0100

    dos actividades

---
+++ actividades_tema11_1al5.sql
@@ -0,0 +1,122 @@
+use pedidos_dam;
+
+/* 1. Crea un procedimiento que añada un nuevo pedido a la tabla Pedido con datos pasados como
+parámetro. Por tanto, este procedimiento recibirá como parámetros la referencia del pedido y la
+fecha. Muestra al final un mensaje con el texto ‘Se ha añadido un pedido con referencia XXXXX’,
+siendo XXXXX la referencia del nuevo pedido.*/
+
+Delimiter //
+create procedure addPedido (referencia char(5), fecha date)
+begin
+insert into pedido
+values (referencia, fecha);
+select concat('Se ha añadido un pedido con referencia', referencia) mensaje;
+end;//
+
+call addPedido('P0078', current_date());
+
+/*2. Escribe un procedimiento que muestre en pantalla la descripción y el precio del artículo más
+barato de la base de datos.*/
+select PVPArt
+from articulo
+order by PVPArt
+asc
+limit 1;
+
+select DesArt 
+from articulo
+where PVPArt = (select PVPArt
+					from articulo
+					order by PVPArt
+					asc
+					limit 1);
+ 
+ Delimiter //
+ create procedure showCheap()
+ begin
+ declare cheap decimal(6,2);
+ declare desCheap varchar(30);
+ select PVPArt into cheap
+	from articulo
+	order by PVPArt
+	asc
+	limit 1;
+select DesArt into desCheap
+	from articulo
+	where PVPArt = (select PVPArt
+						from articulo
+						order by PVPArt
+						asc
+						limit 1);
+select concat('El articulo mas barato es ', desCheap, ' y cuesta ', cheap) mensaje;
+end;//
+
+call showCheap();
+
+/*3. Crea una función que nos devuelva la descripción del artículo más caro de la base de datos.*/
+
+Delimiter //
+ create function showExpensive() returns varchar(30) reads sql data
+ begin
+ declare expensive decimal(6,2);
+ declare desExp varchar(30);
+ select PVPArt into expensive
+	from articulo
+	order by PVPArt
+	desc
+	limit 1;
+select DesArt into desExp
+	from articulo
+	where PVPArt = (select PVPArt
+						from articulo
+						order by PVPArt
+						desc
+						limit 1);
+return desExp;
+end;//
+
+select showExpensive() 'Articulo mas caro';
+
+/*4. Crea un procedimiento que reciba la referencia de un pedido y muestre en pantalla dicha referencia
+y la fecha del pedido y se encargue de eliminarlo de la base de datos. Debe mostrase un mensaje
+con el texto ‘Pedido XXXXX eliminado’.*/
+Delimiter //
+create procedure removePedido (referencia char(5))
+begin
+declare fecha date;
+select FecPed into fecha
+	from pedido
+    where RefPed like referencia;
+select concat('Pedido ', referencia, ' con fecha ',fecha, ' encontrado') 'Datos pedido';
+
+delete from pedido
+where RefPed like referencia;
+select concat('Pedido ',referencia,' eliminado') mensaje2;
+end;//
+
+call removePedido('P0078');
+
+/*5. Crea un procedimiento que reciba el código de un artículo y un número entero positivo o negativo.
+El procedimiento debe modificar el precio del artículo según el porcentaje pasado como parámetro
+y mostrar el precio del artículo antes y después de la modificación. Por ejemplo, si el
+procedimiento recibe como segundo parámetro un 5, deberá subir el precio del artículo un 5%,
+mientras que si recibe un -2, deberá bajar el precio del artículo un 2%. */
+Delimiter //
+create procedure modifyPrice (cod char(5), delta int)
+begin
+declare previous decimal(6,2);
+declare actual decimal(6,2);
+select PVPArt into previous
+	from articulo
+    where CodArt like cod;
+select concat('Se modificara ',cod, ' ,precio anterior: ', previous) mensaje;
+update articulo
+set PVPArt = PVPArt + (PVPArt)*(delta/100)
+	where CodArt like cod;
+select PVPArt into actual
+	from articulo
+    where CodArt like cod;
+select concat('Se modifico ',cod, ' ,precio actual: ', actual) mensaje2;
+end;//
+
+call modifyPrice('A0089', -5);
---
+++ actividades_tema11_6al11.sql
@@ -0,0 +1,101 @@
+use empresa_dam;
+/*6. Crea una función que reciba el número de un departamento y devuelva un número real que indique
+el porcentaje que supone la suma de los salarios de los empleados de ese departamento en relación
+con la suma salarial de todos los empleados de la empresa.*/
+select Salario, NumDep
+from empleado 
+where NumDep = 1;
+
+select SUM(Salario) as sumSalario
+from empleado
+where NumDep = 1
+group by NumDep;
+
+select SUM(Salario)
+from empleado;
+
+Delimiter //
+create function porSalarioDep(dep int) returns int reads sql data
+begin
+declare salDep int;
+declare salALL int;
+declare percent int;    
+select sum(Salario) into salDep
+	from empleado
+    where NumDep = dep
+    group by NumDep;
+    
+select sum(Salario) into salALL
+	from empleado;
+set percent = (salDep/salALL)*100;
+return percent;
+end; //
+    
+select porSalarioDep(1);
+
+/*7. Crea un procedimiento que reciba el nombre de un departamento y su localidad. Se debe insertar
+ese departamento en la tabla Departamento asignándole como número el que resulte de sumar 1
+al número más alto de los departamentos de la empresa. Escribe el final un mensaje con el formato
+‘Departamento no xx añadido’, siendo xx el número asignado al nuevo departamento.*/
+
+select max(NumDep)
+from departamento;
+
+Delimiter //
+create procedure insertDep(nombre varchar(40), localidad varchar(40))
+begin
+declare depAsigned int;
+select max(NumDep) into depAsigned
+	from departamento;
+
+insert into departamento
+values (depAsigned+1, nombre, localidad);
+select concat ('Departamento numero ',depAsigned+1, ' añadido') mensaje;
+end;// 
+
+call insertDep('Relaciones Publicas', 'Sevilla');
+
+/*8. Escribe una función que reciba el número de un empleado de la tabla Empleado y que devuelva
+el número de subordinados que tiene ese empleado, o lo que es lo mismo, el número de empleados
+de los cuales es director o jefe directo. Recuerda que el atributo NumEmpJefe de la tabla Empleado
+indica el número del empleado director o jefe directo. Escribe un ejemplo de llamada a la función.*/
+
+select count(NumEmp)
+	from empleado
+    where NumEmpJefe = 1;
+    
+Delimiter //
+create function Employee(jefe int) returns int reads sql data
+begin
+
+end;//
+
+/*9. Crea un procedimiento llamado ModificarSalariosDep que reciba el número de un departamento
+y un número real con dos decimales. El procedimiento, en primer lugar, obtendrá el número de
+empleados que trabajan en el departamento cuyo número se ha recibido como primer parámetro.
+En caso de que no trabaje ningún empleado en dicho departamento, se mostrará el mensaje “En
+el dpto. no xxx no trabaja ningún empleado”. En caso contrario, se deberá modificar el salario de
+los empleados del departamento cuyo número se ha pasado como primer parámetro en base al
+porcentaje recibido como segundo parámetro, después de lo cual se mostrará el mensaje “Se ha
+modificado el salario de todos los empleados del dpto. no xx en un yyy,yy%”.  */
+
+/*10. Crea un procedimiento llamado AsignarComision que reciba un número de empleado y
+compruebe su salario en la tabla Empleado. Si el salario del empleado es menor que 1500 €, se le
+debe asignar una comisión que será el 5% del salario; en caso de que su salario sea superior o
+igual a 1500 €, pero inferior a 2500 €, se le deberá asignar una comisión igual al 2,5% del salario;
+en caso de que cobre 2500 € o más, se le pondrá a 0 € la comisión. Muestra un mensaje como el
+siguiente: “Al empleado no XXXX se le ha asignado una comisión de YY.YY €”.*/
+
+/*11. Crea una función llamada SalarioJefe que reciba el nombre de un empleado y que devuelva un
+número real que tome el valor del salario del empleado dividido entre el número de empleados
+subordinados que tenga. En caso de que el empleado no tenga subordinados, la función deberá
+devolver el valor -1.*/
+
+/*12. Crea un procedimiento llamado IncrementarComisión que reciba el número de un empleado y
+que en función del número de subordinados de ese empleado se le incremente la comisión del
+siguiente modo: si el empleado no tiene subordinados, se le incrementará la comisión en 10 €; si
+tiene un subordinado, se le incrementará la comisión en 25 €; si tiene 2 subordinados, se le
+incrementará la comisión en 60 €; si tiene 3 subordinados, se le incrementará la comisión en 100
+€; en cualquier otro caso, se le incrementará la comisión en 200 €. Puedes hacer uso de la función
+creada en la actividad 8 para obtener el número de subordinados de un empleado. Muestra al final
+un mensaje indicando el incremento de comisión de que ha disfrutado el empleado*/
